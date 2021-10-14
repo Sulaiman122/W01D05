63c333c22acc0513c1960ab8ae886e2d82cf7292
Sulaiman122 <33493055+Sulaiman122@users.noreply.github.com>
2021-10-14 12:11:08 +0300

    had fun solving them

---
+++ task1/myVariables.js
@@ -0,0 +1,20 @@
+let language = "JavaScript";
+let score = 10;
+
+console.log(language);
+console.log(score);
+
+let length = 32;
+let width = 25;
+
+console.log("The area of the rectangle is " + length * width);
+
+let num1 = 5;
+let num2 = 2;
+
+console.log(`The reminder of ${num1}%${num2} is ` + (num1 % num2));
+
+let x = 1;
+let y = 2;
+console.log(num1 / num2 == Math.trunc(x % y));
+//not sure if this is correct
---
+++ task2/dataTypes.js
@@ -0,0 +1,17 @@
+let x = 4;
+let str = "hello world";
+let arr = [0,1,2,3,"hi"];
+let obj = {name: "Sulaiman",
+age = 26}
+
+console.log(`integer = ${x} -- string = ${str} -- array = ${arr} -- obj = ${obj}`)
+
+console.log("typeof x is " + typeof(x) + " typeof str is " + typeof(str) + " typeof arr is " + typeof(arr) +" typeof obj is " + typeof(obj));
+
+let myArr = [1,2,4,"hi","yes"];
+//I expect it to be typeof object
+
+console.log(typeof(myArr));
+
+let myNull = null;  //null variable
+let myUndefined;    //undefined variable
